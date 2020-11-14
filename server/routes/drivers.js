aaa136b8a5e1dd5bcb5d44726b7b5a2fffa1f633
HamdiBenkahla <68249436+HamdiBenkahla@users.noreply.github.com>
2020-11-14 23:29:43 +0100

    drivers & cars schema, driver function query

--- server/routes/drivers.js
+++ server/routes/drivers.js
@@ -1,1 +1,8 @@
+const db = require('../../database/models/drivers.js')
 
+
+exports.getDrivers =(req, res)=>{
+    db.getDrivers().then(data => {res.status(200).json(data) })
+     .catch(error => res.status(500).send(error))
+  
+  }
